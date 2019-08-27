b0676a4ca392bcf8962d345243bfcc5f1eb5a7d1
Gabriel de la Cruz <gabrieledcjr@gmail.com>
2019-08-27 09:43:15 -0700

    add chapter 2

---
+++ Chapter02/coutpractice.cpp.cpp
@@ -0,0 +1,17 @@
+// coutpractice.cpp
+// Using cout, formatting using escape sequence
+// Gabe de la Cruz
+// CS150 103
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "These are the best hiking trails ";
+    cout << "in CDA" << "\n";
+    cout << "\t\"Tubbs Hill\"\n";
+    cout << "\t\"Mineral Ridge\"\n";
+    cout << "\t\"Marie Creek Trail\"\n";
+    return 0;
+}
---
+++ Chapter02/variables.cpp
@@ -0,0 +1,24 @@
+// variables.cpp
+// Description:
+//    Working with characters. Demonstrate that characters
+//    are internally represented as integers and that
+//    characters are just special integers.
+// Gabe de la Cruz
+// CS150 103
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Declare character variable
+    char letter;
+
+    letter = 65;
+    cout << letter << endl;
+
+    letter = 66;
+    cout << letter << endl;
+
+    return 0;
+}
