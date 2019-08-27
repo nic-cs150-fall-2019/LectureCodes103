2d3e8c857877a0650b6a0c2690e9c9a0704c1ed1
Gabriel de la Cruz <gabrieledcjr@gmail.com>
2019-08-27 09:15:51 -0700

    lecture 1 codes added

---
+++ Lec01/calculatepay.cpp
@@ -0,0 +1,27 @@
+// calculatepay.cpp
+// This program calculates the user's pay
+// Gabe de la Cruz
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double hours, rate, pay;
+
+    // Get the number of hours worked.
+    cout << "How many hours did you work? ";
+    cin >> hours;
+
+    // Get the hourly pay rate.
+    cout << "Hour much do you get paid per hour? ";
+    cin >> rate;
+
+    // Calculate the pay
+    pay = hours * rate;
+
+    // Display the pay.
+    cout << "You have earned $" << pay << endl;
+
+    return 0;
+}
---
+++ Lec01/sample.cpp
@@ -0,0 +1,28 @@
+// sample.cpp
+// sample C++ program
+// comment they are ignored by the complier
+// help other programmers to understand your code
+// to explain what your code does
+/*
+Hi
+Hello
+There
+*/
+
+// # indicates it is preprocessed
+// includes or insert contents of another file
+#include <iostream>
+
+// for name conflicts
+using namespace std;
+
+// function main
+// start here
+int main()
+{
+    // displays message on the screen
+    cout << "Hello ";
+    cout << "World!";
+
+    return 0;
+}
