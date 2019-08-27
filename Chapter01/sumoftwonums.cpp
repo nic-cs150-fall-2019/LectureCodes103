e2ccaa58804ebfa852491bbffcfea0436e00276f
Gabriel de la Cruz <gabrieledcjr@gmail.com>
2019-08-27 09:40:09 -0700

    lectures to chapter

---
+++ Chapter01/sumoftwonums.cpp
@@ -0,0 +1,24 @@
+// sumoftwonums.cpp
+// Display sum of two numbers
+// Gabe de la Cruz
+// CS150 103
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int firstNum = 10;
+    int secondNum = 100;
+    int total = 50l;
+
+    // Get the sum of two numbers
+    total = firstNum + secondNum;
+
+    // Display the total
+    // cout << "The sum of 50 and 100 is " << total;
+    cout << "The sum of " << firstNum << " and " << secondNum << " is " << total;
+    // WRONG: cout << "The sum of firstNum and secondNum is " << total;
+
+    return 0;
+}
